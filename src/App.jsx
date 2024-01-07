416d9a171d5072e26fb6ac73c68667d5c7029172
CosminDarius1 <83471623+CosminDarius1@users.noreply.github.com>
2024-01-07 18:22:31 +0000

    Added functionality inside Tour and Tours components, created removeTour option, also added Read more on tours info

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import {useEffect, useState} from "react";
 import Tour from "./Components/Tour/index.jsx";
 import Loading from "./Components/Loading/index.jsx";
+import Tours from "./Components/Tours/index.jsx";
 
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
   const [isLoading, setisLoading] = useState(true)
   const[tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours)
+  }
 
   const fetchTours = async () => {
     setisLoading(true)
@@ -31,21 +37,16 @@ const App = () => {
     </main>
   }
 
+  if(tours.length === 0) {
+    return <main className='title'>
+      <h2>No tours left</h2>
+      <button type='button' style={{marginTop: '2rem'}} className='btn' onClick={() => fetchTours()}>Refresh</button>
+    </main>
+  }
+
   return <main className="main">
-  <h2>Our tours</h2>
-    <section>
-      {tours.map((tour) => {
-        const {id, name, info, image, price} = tour
-        return <ul key={id} className="tours">
-          <li className="single-tour">
-            <img src={image} alt={info} className="single-tour"/>
-            <h2 className="tour-info">{name}</h2>
-            <h3 className="tour-price">{price}</h3>
-            <p className='tour-info'>{info}</p>
-          </li>
-        </ul>
-      })}
-    </section>
+    <Tours tours={tours} removeTour={removeTour}/>
+
     {/*<Tour/>*/}
   </main>;
 };
